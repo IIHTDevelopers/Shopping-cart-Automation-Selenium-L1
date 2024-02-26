a00854c143b8e239f50e9fcf16bcbdb71f64c07e
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-02-26 18:19:02 +0530

    Add files via upload

--- src/test/java/com/iiht/evaluation/automation/functional/TestAutomation.java
+++ src/test/java/com/iiht/evaluation/automation/functional/TestAutomation.java
@@ -10,6 +10,9 @@ import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,10 +24,12 @@ import com.iiht.evaluation.automation.Activities;
 import com.iiht.evaluation.automation.App;
 import com.iiht.evaluation.automation.testutils.MasterData;
 
+@TestMethodOrder(OrderAnnotation.class)
 public class TestAutomation extends App {
 
     private static WebDriver driver;
 	private static Actions actions;
+	
 	
 	
 	public static String getHrefOfLink(WebElement w)
@@ -39,6 +44,7 @@ public class TestAutomation extends App {
 	}
 
     @Test
+    @Order(1)
     public void test_click_on_shop_menu() throws IOException {
         boolean testcase_status = true;
         try {
@@ -55,6 +61,7 @@ public class TestAutomation extends App {
         }
     }
     @Test
+    @Order(2)
     public void test_click_cart_icon() throws IOException {
         boolean testcase_status = true;
         try {
@@ -72,6 +79,7 @@ public class TestAutomation extends App {
     }
 
     @Test
+    @Order(3)
     public void test_add_current_product_count_in_excel() throws IOException {
         boolean testcase_status = true;
         try {
@@ -91,6 +99,7 @@ public class TestAutomation extends App {
 
 
     @Test
+    @Order(4)
     public void test_arrival_images_add_to_basket_clik_home_menu_button() throws IOException {
         boolean testcase_status = true;
         try {
@@ -110,6 +119,7 @@ public class TestAutomation extends App {
   
 
     @Test
+    @Order(5)
     public void test_click_image_in_arrival() throws IOException {
         boolean testcase_status = true;
         String product_name = MasterData.test_arrival_add_to_basket_items_master_data.get("product_name");
@@ -128,6 +138,7 @@ public class TestAutomation extends App {
     }
 
     @Test
+    @Order(6)
     public void test_click_add_to_basket_button_product_detail_page() throws IOException {
         boolean testcase_status = true;
         try {
@@ -145,6 +156,7 @@ public class TestAutomation extends App {
     }
 
     @Test
+    @Order(7)
     public void test_check_product_added_message_product_detail_page() throws IOException {
         String product_name = MasterData.test_arrival_add_to_basket_items_master_data.get("product_name");
         boolean testcase_status = true;
@@ -163,6 +175,7 @@ public class TestAutomation extends App {
     }
 
     @Test
+    @Order(8)
     public void test_click_item_detail_menu() throws IOException {
         boolean testcase_status = true;
         try {
@@ -180,6 +193,7 @@ public class TestAutomation extends App {
     }
 
     @Test
+    @Order(9)
     public void test_check_checkout_button() throws IOException {
         String product_name = MasterData.test_arrival_add_to_basket_items_master_data.get("product_name");
         boolean testcase_status = true;
